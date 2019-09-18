4dd84c12b3aead0fc463f00fcd97ca0538299f00
NB-Dragon <34410109+NB-Dragon@users.noreply.github.com>
2019-09-18 15:12:56 +0800

    拓展生成headers和cookies字典

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Class.DownloadHelper import DownloadHelper
 import os
 
 
-def make_dict_from_content(content):
+def make_dict_from_cookies(content):
     each_value_list = content.split(";")
     result_dict = {}
     for key_value in each_value_list:
@@ -12,9 +12,19 @@ def make_dict_from_content(content):
     return result_dict
 
 
+def make_dict_from_headers(content):
+    each_value_list = content.split("\n")
+    result_dict = {}
+    for key_value in each_value_list:
+        if key_value == "": continue
+        key, value = key_value.split(":", 1)
+        result_dict[key.strip()] = value.strip()
+    return result_dict
+
+
 if __name__ == '__main__':
-    url = "https://download.sj.qq.com/upload/QQphonemanger/5-8-1/QQPhoneManager_990420.5239.exe"
-    headers = make_dict_from_content('')
-    cookies = make_dict_from_content('')
+    url = "https://download.developer.apple.com/Developer_Tools/Xcode_10.3/Xcode_10.3.xip"
+    headers = make_dict_from_headers('')
+    cookies = make_dict_from_cookies('')
     download_helper = DownloadHelper(url, os.getcwd(), headers, cookies)
     download_helper.start_mission()
