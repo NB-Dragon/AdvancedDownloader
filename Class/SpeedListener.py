1d2e79aa8305859e24a433e86f16e3652e942743
HYA-Machine <34410109+NB-Dragon@users.noreply.github.com>
2020-03-05 10:04:38 +0800

    1:修复由于文件锁导致文件不可读大小的问题 2:变更文件大小的读取方式，采用缓存策略，能读的时候再读

--- Class/SpeedListener.py
+++ Class/SpeedListener.py
@@ -3,7 +3,6 @@
 # Create Time: 2020/2/20 17:10
 # Create User: hya-machine
 import queue
-import re
 import os
 import threading
 import time
@@ -15,34 +14,46 @@ class SpeedListener(threading.Thread):
         self._mission_name = mission_name
         self._file_list = file_list
         self._message_receiver = message_receiver
-        self._file_filter = re.compile("\\.part\\d+$")
         self._run_status = True
+        self._file_info_register = self._init_file_info_register()
 
     def run(self) -> None:
         start_time = time.time()
-        start_position = self._calculate_current_download_size()
+        self._calculate_download_size_change(start_time)
         while self._run_status:
             end_time = time.time()
             sum_time = end_time - start_time
             if sum_time >= 1:
                 start_time = end_time
-                end_position = self._calculate_current_download_size()
-                speed_size = (end_position - start_position) // sum_time
+                speed_size = self._calculate_download_size_change(start_time)
                 speed_description = self._get_format_file_size(speed_size)
-                start_position = end_position
                 self._make_message_and_send(speed_description)
 
     def send_stop_state(self):
         self._run_status = False
 
-    def _calculate_current_download_size(self):
-        current_size = 0
+    def _calculate_download_size_change(self, time_stamp):
+        size_change_in_per_second = 0
+        for file_name_item in self._file_info_register:
+            if os.path.isfile(file_name_item["file_name"]):
+                current_size = os.path.getsize(file_name_item["file_name"])
+                time_length = self._calculate_time_length(file_name_item["update_time"], time_stamp)
+                size_length = current_size - file_name_item["size"]
+                size_change_in_per_second += size_length // time_length
+                file_name_item["size"] = current_size
+                file_name_item["update_time"] = time_stamp
+        return size_change_in_per_second
+
+    @staticmethod
+    def _calculate_time_length(pre_time, now_time):
+        return 1 if pre_time == 0 else now_time - pre_time
+
+    def _init_file_info_register(self):
+        file_info_list = []
         for file_name in self._file_list:
-            if os.path.exists(file_name):
-                current_size += os.path.getsize(file_name)
-            else:
-                self._make_message_and_send("文件不存在：{}".format(file_name))
-        return current_size
+            temp_dict = {"file_name": file_name, "size": 0, "update_time": 0}
+            file_info_list.append(temp_dict)
+        return file_info_list
 
     def _make_message_and_send(self, content):
         message = {"sender": "SpeedListener", "title": self._mission_name, "result": content}
