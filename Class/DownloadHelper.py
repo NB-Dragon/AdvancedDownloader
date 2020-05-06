0ac8270cbff934aafdb39b9df27b1aba5828cf2c
NB-Dragon <34410109+NB-Dragon@users.noreply.github.com>
2020-05-06 11:45:52 +0800

    适配新版信息分发中心机制 新增ID的分发

--- Class/DownloadHelper.py
+++ Class/DownloadHelper.py
@@ -3,12 +3,14 @@ import urllib.parse
 
 
 class DownloadHelper(object):
-    def __init__(self, message_receiver, download_link, save_path, headers: dict = None, cookies: dict = None):
+    def __init__(self, message_receiver, download_link, save_path, download_index,
+                 headers: dict = None, cookies: dict = None):
         self._headers = headers if headers is not None else {}
         self._cookies = cookies if cookies is not None else {}
         self._message_receiver = message_receiver
         self._download_link = download_link
         self._save_path = save_path
+        self._download_index = download_index
         self._create_download_mission()
 
     def _create_download_mission(self):
@@ -16,11 +18,11 @@ class DownloadHelper(object):
         scheme = link_parse_result.scheme
         if scheme in ["https", "http"]:
             heep_helper = HTTPDownloader(self._message_receiver, self._download_link, self._save_path,
-                                         self._headers, self._cookies)
+                                         self._download_index, self._headers, self._cookies)
             heep_helper.start_download_mission()
         else:
-            self._make_message_and_send("unknown scheme, please wait to support!")
+            self._make_message_and_send("unknown scheme, please wait to support!", False)
 
-    def _make_message_and_send(self, content):
-        message = {"sender": "DownloadHelper", "title": "info", "result": content}
-        self._message_receiver.put(message)
+    def _make_message_and_send(self, content, exception):
+        message = {"sender": "DownloadHelper", "title": self._download_index, "content": content}
+        self._message_receiver.put({"message": message, "exception": exception})
