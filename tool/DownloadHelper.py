ab6f9720828b39a71aa02892c6680a235420fe0b
NB-Dragon <34410109+NB-Dragon@users.noreply.github.com>
2021-01-21 10:53:56 +0800

    1：更换http/https下载协议的处理助手 2：适配新版内容分发机制

--- tool/DownloadHelper.py
+++ tool/DownloadHelper.py
@@ -1,28 +1,31 @@
-from schema.HTTPDownloader import HTTPDownloader
+import uuid
 import urllib.parse
+from schema.Downloader.HTTPDownloader import HTTPDownloader
 
 
 class DownloadHelper(object):
-    def __init__(self, message_receiver, download_link, save_path, download_index,
-                 headers: dict = None, cookies: dict = None):
+    def __init__(self, message_receiver, download_link, save_path, headers: dict = None, cookies: dict = None):
         self._headers = headers if headers is not None else {}
         self._cookies = cookies if cookies is not None else {}
+        self._uuid_description = "".join(str(uuid.uuid1()).split("-"))
         self._message_receiver = message_receiver
         self._download_link = download_link
         self._save_path = save_path
-        self._download_index = download_index
         self._create_download_mission()
 
     def _create_download_mission(self):
         link_parse_result = urllib.parse.urlparse(self._download_link)
         scheme = link_parse_result.scheme
         if scheme in ["https", "http"]:
-            http_helper = HTTPDownloader(self._message_receiver, self._download_link, self._save_path,
-                                         self._download_index, self._headers, self._cookies)
-            http_helper.start_download_mission()
+            http_helper = HTTPDownloader(self._download_link, self._save_path, self._uuid_description,
+                                         self._message_receiver, self._headers, self._cookies)
+            http_helper.start_download_mission(128)
         else:
             self._make_message_and_send("unknown scheme, please wait to support!", False)
 
     def _make_message_and_send(self, content, exception: bool):
-        message = {"sender": "DownloadHelper", "title": self._download_index, "content": content}
-        self._message_receiver.put({"message": message, "exception": exception})
+        message_dict = dict()
+        message_dict["action"] = "print"
+        message_dict["value"] = {"sender": "DownloadHelper", "mission_uuid": self._uuid_description,
+                                 "content": content, "exception": exception}
+        self._message_receiver.put(message_dict)
