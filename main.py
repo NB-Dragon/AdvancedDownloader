64c3ceeb1f07c853cc59109b97417873d35feb27
NB-Dragon <34410109+NB-Dragon@users.noreply.github.com>
2021-01-22 15:05:40 +0800

    修改逻辑以及排版，适配新版DownloadHelper

--- main.py
+++ main.py
@@ -2,12 +2,11 @@
 # -*- coding: utf-8 -*-
 # Create Time: 2021/1/20 10:00
 # Create User: anonymous
-
 import os
 from tool.DownloadHelper import DownloadHelper
+from tool.RuntimeOperator import RuntimeOperator
 from schema.RequestDictionary import RequestDictionary
 from listener.ThreadMessageDistributor import ThreadMessageDistributor
-from tool.RuntimeOperator import RuntimeOperator
 
 if __name__ == '__main__':
     runtime_operator = RuntimeOperator()
@@ -15,11 +14,14 @@ if __name__ == '__main__':
     thread_message_distributor = ThreadMessageDistributor(runtime_operator)
     thread_message_queue = thread_message_distributor.get_message_queue()
     thread_message_distributor.start()
+    download_helper = DownloadHelper(thread_message_queue)
 
     headers = RequestDictionary.make_dict_from_headers("")
     cookies = RequestDictionary.make_dict_from_cookies("")
     # url = "https://speedtest3.gd.chinamobile.com.prod.hosts.ooklaserver.net:8080/download?size=1073741824"
     url = "https://github.com/iBotPeaches/Apktool/releases/download/v2.5.0/apktool_2.5.0.jar"
-    DownloadHelper(thread_message_queue, url, os.getcwd(), headers, cookies)
+    base_info = {"download_link": url, "save_path": os.getcwd(), "thread_num": 128}
+    base_info.update({"headers": headers, "cookies": cookies})
+    download_helper.create_new_download_mission(base_info)
 
     thread_message_distributor.send_stop_state()
