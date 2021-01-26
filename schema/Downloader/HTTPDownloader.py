e567753ee6a2c51885318a805bf01b7f87b1ea14
NB-Dragon <34410109+NB-Dragon@users.noreply.github.com>
2021-01-26 19:03:21 +0800

    移除requests的支持

--- schema/Downloader/HTTPDownloader.py
+++ schema/Downloader/HTTPDownloader.py
@@ -1,6 +1,5 @@
 import os
 import queue
-import requests
 import threading
 from schema.Analyser.HTTPHelper import HTTPHelper
 
@@ -9,16 +8,16 @@ class HTTPDownloader(object):
     def __init__(self, mission_uuid, mission_info: dict, download_info: dict, thread_message: queue.Queue):
         self._mission_uuid = mission_uuid
         self._mission_info = mission_info
-        # mission_info = {"download_link": str, "save_path": str, "thread_num": 128, "headers": dict, "cookies": dict}
-        self._thread_message = thread_message
-        self._download_config = download_info
+        # mission_info = {"download_link": str, "save_path": str, "thread_num": 128, "headers": dict}
+        self._download_info = download_info
         # download_info = {"file_info": dict, "all_region": list[list], "tmp_path": str}
-        self._mission_lock = threading.Lock()
-        self._mission_lock.acquire()
+        self._thread_message = thread_message
+        self._request_pool = self._init_request_pool()
+        self._mission_lock = self._init_mission_lock()
 
     def start_download_mission(self):
         self._try_to_update_mission_info()
-        if self._download_config["file_info"]:
+        if self._download_info["file_info"]:
             self._create_download_tmp_file()
             self._send_download_mission_register()
             # do something here
@@ -30,33 +29,42 @@ class HTTPDownloader(object):
         else:
             self._make_message_and_send("资源禁止访问，请确认验证信息", False)
 
+    def _init_request_pool(self):
+        thread_count = self._mission_info["thread_num"]
+        return HTTPHelper.get_request_pool_manager(thread_count)
+
+    @staticmethod
+    def _init_mission_lock():
+        lock = threading.Lock()
+        lock.acquire()
+        return lock
+
     def _try_to_update_mission_info(self):
         self._make_message_and_send("资源连接中", False)
-        if len(self._download_config) == 0:
-            self._download_config["file_info"] = self._analyse_target_file_info()
-            self._download_config["all_region"] = self._generate_file_all_region(self._download_config["file_info"])
-            self._download_config["tmp_path"] = self._generate_tmp_file_path(self._download_config["file_info"])
+        if len(self._download_info) == 0:
+            self._download_info["file_info"] = self._analyse_target_file_info()
+            self._download_info["all_region"] = self._generate_file_all_region(self._download_info["file_info"])
+            self._download_info["tmp_path"] = self._generate_tmp_file_path(self._download_info["file_info"])
 
     def _create_download_tmp_file(self):
         self._make_message_and_send("正在初始化", False)
-        writer = open(self._download_config["tmp_path"], 'a+b')
-        file_size = self._download_config["file_info"]["filesize"]
-        file_buff = self._get_empty_byte_array(self._download_config["tmp_path"], file_size)
+        writer = open(self._download_info["tmp_path"], 'a+b')
+        file_size = self._download_info["file_info"]["filesize"]
+        file_buff = self._get_empty_byte_array(self._download_info["tmp_path"], file_size)
         writer.write(file_buff)
         writer.close()
 
     def _rename_final_save_file(self):
         self._make_message_and_send("文件整合中", False)
-        file_info = self._download_config["file_info"]
+        file_info = self._download_info["file_info"]
         save_file = os.path.join(self._mission_info["save_path"], file_info["filename"])
-        os.rename(self._download_config["tmp_path"], save_file)
+        os.rename(self._download_info["tmp_path"], save_file)
 
     def _analyse_target_file_info(self):
         tmp_headers = self._mission_info["headers"].copy()
         tmp_headers["Range"] = "bytes=0-0"
-        tmp_cookies = self._mission_info["cookies"].copy()
         for try_time in range(3):
-            stream_response = self._get_simple_response(self._mission_info["download_link"], tmp_headers, tmp_cookies)
+            stream_response = self._get_simple_response(self._mission_info["download_link"], tmp_headers)
             if self._check_response_can_access(stream_response):
                 headers = {key.lower(): value for key, value in dict(stream_response.headers).items()}
                 stream_response.close()
@@ -90,15 +98,15 @@ class HTTPDownloader(object):
     def _check_response_can_access(stream_response):
         if stream_response is None:
             return False
-        if stream_response.status_code in [200, 206]:
+        if stream_response.status in [200, 206]:
             return True
         else:
             stream_response.close()
             return False
 
-    def _get_simple_response(self, target_url, headers, cookies):
+    def _get_simple_response(self, target_url, headers):
         try:
-            return requests.get(target_url, stream=True, timeout=10, headers=headers, cookies=cookies)
+            return self._request_pool.request("GET", target_url, headers=headers, preload_content=False, timeout=1)
         except Exception as e:
             self._make_message_and_send(str(e), True)
             return None
@@ -107,7 +115,7 @@ class HTTPDownloader(object):
         message_dict = dict()
         message_dict["action"] = "write"
         detail_info = {"type": "register", "lock": self._mission_lock}
-        detail_info.update({"mission_info": self._mission_info, "download_info": self._download_config})
+        detail_info.update({"mission_info": self._mission_info, "download_info": self._download_info})
         message_dict["value"] = {"mission_uuid": self._mission_uuid, "detail": detail_info}
         self._thread_message.put(message_dict)
 
