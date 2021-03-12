1a331ac4026819333e39396cd4b596ab2061cc38
NB-Dragon <34410109+NB-Dragon@users.noreply.github.com>
2021-03-12 12:03:37 +0800

    1：修改默认请求头 2：新增百度cookie头的生成

--- schema/RequestDictionary.py
+++ schema/RequestDictionary.py
@@ -1,9 +1,15 @@
 class RequestDictionary(object):
     @staticmethod
-    def get_default_user_agent():
+    def get_header_user_agent():
         user_agent_list = list()
-        user_agent_list.append("Mozilla/5.0")
+        user_agent_list.append("Mozilla/5.0 (X11; Linux x86_64)")
+        user_agent_list.append("AppleWebKit/537.36 (KHTML, like Gecko)")
+        user_agent_list.append("AdvancedDownloader/0.5.4")
         return {"User-Agent": " ".join(user_agent_list)}
+
+    @staticmethod
+    def get_header_baidu_cookie(bduss: str):
+        return {"Cookie": "BDUSS={}".format(bduss)}
 
     @staticmethod
     def make_dict_from_headers(content):
