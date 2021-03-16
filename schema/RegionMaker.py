fcc49908fd851549adfb1a37db7ccb81d8712829
NB-Dragon <34410109+NB-Dragon@users.noreply.github.com>
2021-03-16 15:16:24 +0800

    重新调整区间生成器的逻辑，提高代码可读性

--- schema/RegionMaker.py
+++ schema/RegionMaker.py
@@ -3,32 +3,51 @@
 # Create Time: 2021/3/15 20:00
 # Create User: NB-Dragon
 from math import ceil
+from typing import List
 
 
 class RegionMaker(object):
-    def get_download_region(self, current_region_list: list, spare_worker_count: int):
-        assert_content = "The length of `current_region_list` needs to be less than or equal to `spare_worker_count`"
+    def get_download_region(self, current_region_list: List[List[int]], spare_worker_count: int):
+        """
+            @:param current_region_list: [[min1, max1], [min2, max2], ...];
+
+            @:param spare_worker_count: spare_worker_count > 0;
+
+            @:argument len(current_region_list) <= spare_worker_count.
+        """
+        assert_content = "require len(current_region_list) <= spare_worker_count."
         assert len(current_region_list) <= spare_worker_count, assert_content
-        grant_region_dict = self._get_granted_num_by_region_list(current_region_list, spare_worker_count)
+        sorted_list = sorted(current_region_list, key=lambda x: x[1] - x[0], reverse=True)
+        region_capacity_list = [x[1] - x[0] + 1 for x in sorted_list]
+        granted_list = self._generate_new_capacity_with_weight(region_capacity_list, spare_worker_count)
+        return self._generate_new_region_with_capacity(sorted_list, granted_list)
+
+    def _generate_new_capacity_with_weight(self, capacity_weight_list, weight_maximum):
+        average_number = ceil(sum(capacity_weight_list) / weight_maximum)
+        capacity_list = [ceil(x / average_number) for x in capacity_weight_list]
+        sum_of_weight = sum(capacity_list)
+        while sum_of_weight > weight_maximum:
+            number_appearance_dict = self._make_appearance_dict(capacity_list)
+            two_max_number = sorted(set(capacity_list), reverse=True)[0:2]
+            sub_between_list = two_max_number[0] - two_max_number[1]
+            sub_between_num = sum_of_weight - weight_maximum
+            real_to_sub = min(sub_between_list, sub_between_num)
+            sub_operate_list = self._split_download_size(real_to_sub, number_appearance_dict[two_max_number[0]])
+            for index in range(len(sub_operate_list)):
+                capacity_list[index] -= sub_operate_list[index]
+            capacity_list.sort(reverse=True)
+            sum_of_weight = sum(capacity_list)
+        return capacity_list
+
+    def _generate_new_region_with_capacity(self, region_list, granted_list):
         result_list = list()
-        for index in range(len(current_region_list)):
-            operate_item = grant_region_dict["sorted_list"][index]
-            operate_count = grant_region_dict["granted_list"][index]
+        for index in range(len(region_list)):
+            operate_item = region_list[index]
+            operate_count = granted_list[index]
             result_list.extend(self._split_download_region(operate_item, operate_count))
         return sorted(result_list, key=lambda x: x[0])
 
-    def _get_granted_num_by_region_list(self, current_region_list: list, maximum: int):
-        sorted_by_region_length = sorted(current_region_list, key=lambda x: x[1] - x[0], reverse=True)
-        sum_of_region_list = [x[1] - x[0] + 1 for x in sorted_by_region_length]
-        average_of_full_size = ceil(sum(sum_of_region_list) / maximum)
-        result_list = self._average_distribute_by_capacity(sum_of_region_list, average_of_full_size, maximum)
-        return {"sorted_list": sorted_by_region_length, "granted_list": result_list}
-
-    def _split_download_region(self, current_region: list, thread_count: int):
-        """
-            @:param current_region: [min, max]; 0 <= min, max
-            @:param thread_count: 0 < thread_count
-        """
+    def _split_download_region(self, current_region: List[int], thread_count: int):
         content_size = current_region[1] - current_region[0] + 1
         each_small_region_size = self._split_download_size(content_size, thread_count)
         current_position = current_region[0]
@@ -38,22 +57,6 @@ class RegionMaker(object):
             result_list.append([current_position, end_position - 1])
             current_position = end_position
         return result_list
-
-    def _average_distribute_by_capacity(self, capacity_weight_list: list, average_number, total_distribution):
-        times_of_weight_list = [ceil(x / average_number) for x in capacity_weight_list]
-        sum_of_number = sum(times_of_weight_list)
-        while sum_of_number > total_distribution:
-            number_appearance_dict = self._make_appearance_dict(times_of_weight_list)
-            two_max_number = sorted(set(times_of_weight_list), reverse=True)[0:2]
-            sub_between_list = two_max_number[0] - two_max_number[1]
-            sub_between_num = sum_of_number - total_distribution
-            real_to_sub = min(sub_between_list, sub_between_num)
-            sub_operate_list = self._split_download_size(real_to_sub, number_appearance_dict[two_max_number[0]])
-            for index in range(len(sub_operate_list)):
-                times_of_weight_list[index] -= sub_operate_list[index]
-            times_of_weight_list = sorted(times_of_weight_list, reverse=True)
-            sum_of_number = sum(times_of_weight_list)
-        return list(times_of_weight_list)
 
     @staticmethod
     def _make_appearance_dict(number_list):
