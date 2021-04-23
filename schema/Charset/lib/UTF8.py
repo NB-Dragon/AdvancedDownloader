2d44222ce35eac8908c494b6716f9d35436234f3
NB-Dragon <34410109+NB-Dragon@users.noreply.github.com>
2021-04-23 13:28:38 +0800

    改进检测函数结构，便于其他字符集的模仿

--- schema/Charset/lib/UTF8.py
+++ schema/Charset/lib/UTF8.py
@@ -11,7 +11,6 @@ class UTF8(object):
         self._init_specification()
 
     def _init_specification(self):
-        self._rule_list.append({"regex": b"[\x00-\x7F]", "length": 1})
         self._rule_list.append({"regex": b"[\xC0-\xDF][\x80-\xBF]{1}", "length": 2})
         self._rule_list.append({"regex": b"[\xE0-\xEF][\x80-\xBF]{2}", "length": 3})
         self._rule_list.append({"regex": b"[\xF0-\xF7][\x80-\xBF]{3}", "length": 4})
@@ -19,8 +18,31 @@ class UTF8(object):
         self._rule_list.append({"regex": b"[\xFC-\xFD][\x80-\xBF]{5}", "length": 6})
 
     def detect(self, byte_string: bytes):
-        match_length = 0
+        expect_length, match_length = len(byte_string), 0
+        byte_string_template = self._generate_bytes_template(byte_string)
+        for tmp_byte_string in byte_string_template:
+            tmp_match_count = self._detect_match_count(tmp_byte_string)
+            if tmp_match_count > match_length:
+                match_length = tmp_match_count
+        match_length += self._get_lower_count(byte_string)
+        return match_length / expect_length
+
+    def _generate_bytes_template(self, byte_string: bytes):
+        result_list = list()
+        result_list.append(self._get_bytes_without_lower(byte_string))
+        return result_list
+
+    def _detect_match_count(self, byte_string: bytes):
+        match_count = 0
         for rule in self._rule_list:
             rule_match_length = len(re.findall(rule["regex"], byte_string))
-            match_length += rule["length"] * rule_match_length
-        return match_length / len(byte_string)
+            match_count += rule["length"] * rule_match_length
+        return match_count
+
+    @staticmethod
+    def _get_lower_count(byte_string: bytes):
+        return len(re.findall(b"[\x00-\x7F]", byte_string))
+
+    @staticmethod
+    def _get_bytes_without_lower(byte_string: bytes):
+        return b"".join(re.findall(b"[^\x00-\x7F]", byte_string))
