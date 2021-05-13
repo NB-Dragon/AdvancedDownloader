ffd19e38607e3b42fd2182e017d2ce8f6ab5c0a2
NB-Dragon <34410109+NB-Dragon@users.noreply.github.com>
2021-05-13 21:21:24 +0800

    完善事件"open|start|pause|delete"的逻辑 待完成函数_create_mission_thread

--- manager/MissionManager.py
+++ manager/MissionManager.py
@@ -9,12 +9,15 @@ from listener.ThreadMessageDistributor import ThreadMessageDistributor
 
 
 class MissionManager(threading.Thread):
-    def __init__(self):
+    def __init__(self, runtime_operator: RuntimeOperator):
         super().__init__()
-        self._runtime_operator = RuntimeOperator()
+        self._runtime_operator = runtime_operator
         self._message_queue = queue.Queue()
         self._run_status = True
         self._mission_dict = dict()
+        # mission_dict = {"mission_uuid": {"mission_info": dict, "download_info": dict, "running": bool}}
+        self._thread_and_action_dict = dict()
+        # thread_and_action_dict = {"mission_uuid": {"thread": threading.Thread, "action":[]}}
         self._init_all_listener()
 
     def run(self) -> None:
@@ -22,7 +25,12 @@ class MissionManager(threading.Thread):
         while self._should_thread_continue_to_execute():
             message_dict = self._message_queue.get()
             if message_dict is None: continue
-
+            message_detail = message_dict["detail"]
+            if message_dict["action"] == "operate":
+                self._handle_operate_action(message_detail["mission_uuid"], message_detail["detail"])
+            elif message_dict["action"] == "signal":
+                self._handle_thread_signal(message_detail["mission_uuid"], message_detail["signal"])
+            self._process_the_first_action(message_detail["mission_uuid"])
         self._stop_all_listener()
 
     def get_message_queue(self):
@@ -34,6 +42,82 @@ class MissionManager(threading.Thread):
 
     def _should_thread_continue_to_execute(self):
         return self._run_status or self._message_queue.qsize() or self._has_mission_alive()
+
+    def _has_mission_alive(self):
+        alive_mission = [key for key, value in self._mission_dict.items() if value["running"]]
+        return len(alive_mission) == 0
+
+    def _handle_operate_action(self, mission_uuid, mission_detail):
+        handle_type = mission_detail["type"]
+        if handle_type == "create":
+            self._mission_dict[mission_uuid] = mission_detail["value"]
+        elif handle_type == "open":
+            self._thread_and_action_dict[mission_uuid]["action"].append("open")
+        elif handle_type == "start":
+            self._thread_and_action_dict[mission_uuid]["action"].append("start")
+        elif handle_type == "pause":
+            self._thread_and_action_dict[mission_uuid]["action"].append("pause")
+        elif handle_type == "delete":
+            self._thread_and_action_dict[mission_uuid]["action"].append("delete")
+
+    def _handle_thread_signal(self, mission_uuid, signal_type):
+        if signal_type == "stop":
+            self._synchronize_mission_stop_state(mission_uuid)
+            self._synchronize_mission_download_info(mission_uuid)
+        elif signal_type == "finish":
+            self._synchronize_mission_stop_state(mission_uuid)
+            self._append_action_open_if_needed(mission_uuid)
+
+    def _process_the_first_action(self, mission_uuid):
+        action_list = self._thread_and_action_dict[mission_uuid]["action"]
+        if len(action_list):
+            current_action = action_list.pop(0)
+            if current_action == "open":
+                self._do_with_signal_open(mission_uuid)
+            elif current_action == "start":
+                self._do_with_signal_start(mission_uuid)
+            elif current_action == "pause":
+                self._do_with_signal_pause(mission_uuid)
+            elif current_action == "delete":
+                self._do_with_signal_delete(mission_uuid)
+
+    def _do_with_signal_open(self, mission_uuid):
+        sava_file = self._mission_dict[mission_uuid]["download_info"]["save_path"]
+        message_dict = dict()
+        message_dict["action"] = "open"
+        detail_info = {"type": "open", "path": sava_file}
+        message_dict["value"] = {"mission_uuid": mission_uuid, "detail": detail_info}
+        self._all_listener["message"]["queue"].put(message_dict)
+
+    def _do_with_signal_start(self, mission_uuid):
+        self._thread_and_action_dict[mission_uuid]["thread"] = self._create_mission_thread(mission_uuid)
+        self._thread_and_action_dict[mission_uuid]["thread"].start()
+        self._mission_dict[mission_uuid]["running"] = True
+
+    def _do_with_signal_pause(self, mission_uuid):
+        if self._thread_and_action_dict[mission_uuid]["thread"]:
+            self._thread_and_action_dict[mission_uuid]["thread"].send_stop_state()
+
+    def _do_with_signal_delete(self, mission_uuid):
+        if self._mission_dict[mission_uuid]["running"]:
+            self._thread_and_action_dict[mission_uuid]["action"].append("delete")
+            self._do_with_signal_pause(mission_uuid)
+        else:
+            self._mission_dict.pop(mission_uuid)
+            self._thread_and_action_dict.pop(mission_uuid)
+
+    def _synchronize_mission_stop_state(self, mission_uuid):
+        self._mission_dict[mission_uuid]["running"] = False
+        self._thread_and_action_dict[mission_uuid]["thread"] = None
+
+    def _synchronize_mission_download_info(self, mission_uuid):
+        history_mission_dict = self._runtime_operator.get_mission_state()
+        self._mission_dict[mission_uuid]["download_info"] = history_mission_dict[mission_uuid]["download_info"]
+
+    def _append_action_open_if_needed(self, mission_uuid):
+        current_mission_info = self._mission_dict[mission_uuid]["mission_info"]
+        if current_mission_info["open_after_finish"]:
+            self._thread_and_action_dict[mission_uuid]["action"].append("open")
 
     def _init_all_listener(self):
         """
@@ -51,7 +135,3 @@ class MissionManager(threading.Thread):
     def _stop_all_listener(self):
         for listener in self._all_listener.values():
             listener["receiver"].send_stop_state()
-
-    def _has_mission_alive(self):
-        alive_mission = [key for key, value in self._mission_dict.items() if value["running"]]
-        return len(alive_mission) == 0
