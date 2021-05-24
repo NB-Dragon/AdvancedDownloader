cd1cac34756342e5ea83dbe6b67754be44835b53
NB-Dragon <34410109+NB-Dragon@users.noreply.github.com>
2021-05-24 11:13:15 +0800

    跨层级通信新增约束，丢弃未知接收器

--- listener/ThreadMessageDistributor.py
+++ listener/ThreadMessageDistributor.py
@@ -28,9 +28,9 @@ class ThreadMessageDistributor(threading.Thread):
             message_dict = self._message_queue.get()
             if message_dict is None: continue
             if "." in message_dict["receiver"]:
-                self._send_cross_level_message(message_dict)
+                self._handle_cross_level_receiver(message_dict)
             else:
-                self._handle_action_signal(message_dict["receiver"], message_dict["value"])
+                self._handle_same_level_receiver(message_dict["receiver"], message_dict["value"])
         self._do_before_distributor_down()
         self._stop_all_listener()
 
@@ -44,23 +44,23 @@ class ThreadMessageDistributor(threading.Thread):
     def _should_thread_continue_to_execute(self):
         return self._run_status or self._message_queue.qsize()
 
-    def _send_cross_level_message(self, raw_message):
+    def _handle_cross_level_receiver(self, raw_message):
         first_receiver, next_receiver = raw_message["receiver"].split(".", 1)
         raw_message["receiver"] = next_receiver
         if first_receiver == "parent":
             self._parent_queue.put(raw_message)
-        else:
-            self._all_listener[first_receiver]["queue"].put(raw_message)
+        elif first_receiver in self._all_receiver:
+            self._all_receiver[first_receiver]["queue"].put(raw_message)
 
-    def _handle_action_signal(self, signal_receiver, signal_detail):
-        if signal_receiver in self._all_listener:
-            self._all_listener[signal_receiver]["queue"].put(signal_detail)
+    def _handle_same_level_receiver(self, signal_receiver, signal_detail):
+        if signal_receiver in self._all_receiver:
+            self._all_receiver[signal_receiver]["queue"].put(signal_detail)
         else:
             message_content = "signal_receiver `{}` not defined".format(signal_receiver)
             self._send_message_to_print(message_content, False)
 
     def _do_before_distributor_down(self):
-        if not self._all_listener["open"]["receiver"].is_command_installed():
+        if not self._all_receiver["open"]["receiver"].is_command_installed():
             file_path = self._runtime_operator.get_static_donate_image_path()
             message_content = "The sponsored QR code image path is: {}".format(file_path)
             self._send_message_to_print(message_content, False)
@@ -73,37 +73,37 @@ class ThreadMessageDistributor(threading.Thread):
         speed   : handle all the changes in file size
         analyze : handle all the mission analyze
         """
-        self._all_listener = dict()
+        self._all_receiver = dict()
         action_print_receiver = ActionPrintReceiver(self._runtime_operator)
         action_print_queue = action_print_receiver.get_message_queue()
-        self._all_listener["print"] = {"receiver": action_print_receiver, "queue": action_print_queue}
+        self._all_receiver["print"] = {"receiver": action_print_receiver, "queue": action_print_queue}
         action_write_receiver = ActionWriterReceiver(self._runtime_operator, self._message_queue)
         action_write_queue = action_write_receiver.get_message_queue()
-        self._all_listener["write"] = {"receiver": action_write_receiver, "queue": action_write_queue}
+        self._all_receiver["write"] = {"receiver": action_write_receiver, "queue": action_write_queue}
         action_open_receiver = ActionOpenReceiver(self._runtime_operator, self._message_queue)
         action_open_queue = action_open_receiver.get_message_queue()
-        self._all_listener["open"] = {"receiver": action_open_receiver, "queue": action_open_queue}
+        self._all_receiver["open"] = {"receiver": action_open_receiver, "queue": action_open_queue}
         self._analyze_controller = AnalyzeController()
         action_speed_receiver = ActionSpeedReceiver(self._runtime_operator, self._message_queue)
         action_speed_receiver.set_analyze_controller(self._analyze_controller)
         action_speed_queue = action_speed_receiver.get_message_queue()
-        self._all_listener["speed"] = {"receiver": action_speed_receiver, "queue": action_speed_queue}
+        self._all_receiver["speed"] = {"receiver": action_speed_receiver, "queue": action_speed_queue}
         action_analyze_receiver = ActionAnalyzeReceiver(self._runtime_operator, self._message_queue)
         action_analyze_receiver.set_analyze_controller(self._analyze_controller)
         action_analyze_queue = action_analyze_receiver.get_message_queue()
-        self._all_listener["analyze"] = {"receiver": action_analyze_receiver, "queue": action_analyze_queue}
+        self._all_receiver["analyze"] = {"receiver": action_analyze_receiver, "queue": action_analyze_queue}
 
     def _start_all_listener(self):
-        for listener in self._all_listener.values():
+        for listener in self._all_receiver.values():
             listener["receiver"].start()
 
     def _stop_all_listener(self):
-        for listener in self._all_listener.values():
+        for listener in self._all_receiver.values():
             listener["receiver"].send_stop_state()
 
     def _send_message_to_print(self, content, exception: bool):
         message_item = self._generate_print_value(content, exception)
-        self._all_listener["print"]["queue"].put(message_item)
+        self._all_receiver["print"]["queue"].put(message_item)
 
     @staticmethod
     def _generate_print_value(content, exception: bool):
