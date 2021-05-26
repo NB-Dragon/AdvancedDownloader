4a9e34ef8e9e0ac6e4f40d5ab67f1c192936cc24
NB-Dragon <34410109+NB-Dragon@users.noreply.github.com>
2021-05-26 14:38:01 +0800

    重新设计新版本的入口启动逻辑

--- main.py
+++ main.py
@@ -2,23 +2,13 @@
 # -*- coding: utf-8 -*-
 # Create Time: 2021/1/20 10:00
 # Create User: anonymous
-import os
-from tools.DownloadHelper import DownloadHelper
+from manager.MissionActionDistributor import MissionActionDistributor
 from tools.RuntimeOperator import RuntimeOperator
-from schema.analyser.HTTPHelper import HeaderGenerator
-from listener.ThreadMessageDistributor import ThreadMessageDistributor
 
 if __name__ == '__main__':
     runtime_operator = RuntimeOperator()
-    thread_message_distributor = ThreadMessageDistributor(runtime_operator)
-    thread_message_queue = thread_message_distributor.get_message_queue()
-    thread_message_distributor.start()
-    download_helper = DownloadHelper(thread_message_queue)
+    mission_action_distributor = MissionActionDistributor(runtime_operator)
+    mission_action_queue = mission_action_distributor.get_message_queue()
+    mission_action_distributor.start()
 
-    headers = HeaderGenerator.get_header_user_agent()
-    # url = "https://vipspeedtest1.wuhan.net.cn:8080/download?size=25000000&r=0.5588543787999258"
-    url = "https://github.com/NB-Dragon/AdvancedDownloader/archive/refs/tags/v0.5.10.zip"
-    base_info = {"download_link": url, "save_path": os.getcwd(), "thread_num": 32, "headers": headers, "open": True}
-    download_helper.create_download_mission(base_info)
-
-    thread_message_distributor.send_stop_state()
+    mission_action_queue.put(None)
