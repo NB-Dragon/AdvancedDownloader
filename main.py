8cfc4a03edd28f05d416f61a34d3c46ed53e5da5
NB-Dragon <34410109+NB-Dragon@users.noreply.github.com>
2021-05-27 12:23:11 +0800

    修改入口启动逻辑，允许用户通过输入指令，调试执行任务

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 # Create Time: 2021/1/20 10:00
 # Create User: anonymous
+import json
+import sys
 from manager.MissionActionDistributor import MissionActionDistributor
 from tools.RuntimeOperator import RuntimeOperator
 
@@ -11,4 +13,9 @@ if __name__ == '__main__':
     mission_action_queue = mission_action_distributor.get_message_queue()
     mission_action_distributor.start()
 
-    mission_action_queue.put(None)
+    while True:
+        content = sys.stdin.readline()
+        if len(content) <= 1:
+            break
+        mission_action_queue.put(json.loads(content[:-1]))
+    mission_action_distributor.send_stop_state()
