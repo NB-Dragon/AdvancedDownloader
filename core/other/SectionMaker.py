e866c55761af42db36bf99fada8affe2764adc3b
NB-Dragon <34410109+NB-Dragon@users.noreply.github.com>
2021-08-09 19:25:44 +0800

    1: 兼容当前区间数量比期望生成数量要多的情况 2: 优化部分逻辑，提高可读性

--- core/other/SectionMaker.py
+++ core/other/SectionMaker.py
@@ -6,56 +6,50 @@ from math import ceil
 
 
 class SectionMaker(object):
-    def get_download_section(self, current_section_list, expect_distribute_count: int):
+    def get_download_section(self, current_section_list, expect_section_count: int):
         """
             @:param current_section_list: [[min1, max1], [min2, max2], ...];
 
-            @:param expect_distribute_count: expect_distribute_count > 0;
-
-            @:argument len(current_section_list) <= expect_distribute_count.
+            @:param expect_section_count: expect_section_count > 0.
         """
-        assert_content = "require len(current_section_list) <= expect_distribute_count."
-        assert len(current_section_list) <= expect_distribute_count, assert_content
-        sorted_list = sorted(current_section_list, key=lambda x: x[1] - x[0], reverse=True)
-        section_capacity_list = [x[1] - x[0] + 1 for x in sorted_list]
-        granted_list = self._generate_new_capacity_with_weight(section_capacity_list, expect_distribute_count)
-        return self._generate_new_section_with_capacity(sorted_list, granted_list)
+        expect_section_count = self._format_final_section_count(len(current_section_list), expect_section_count)
+        sorted_section_list = sorted(current_section_list, key=lambda x: x[1] - x[0], reverse=True)
+        section_size_list = [self._get_section_size(item) for item in sorted_section_list]
+        granted_list = self._generate_new_capacity_with_weight(section_size_list, expect_section_count)
+        return self._generate_new_section_with_capacity(sorted_section_list, granted_list)
 
-    def _generate_new_capacity_with_weight(self, capacity_weight_list, weight_maximum):
-        average_number = ceil(sum(capacity_weight_list) / weight_maximum)
+    def _generate_new_capacity_with_weight(self, capacity_weight_list, maximum_weight):
+        average_number = ceil(sum(capacity_weight_list) / maximum_weight)
         capacity_list = [ceil(x / average_number) for x in capacity_weight_list]
-        sum_of_weight = sum(capacity_list)
-        while sum_of_weight > weight_maximum:
+        current_weight = sum(capacity_list)
+        while current_weight > maximum_weight:
             number_appearance_dict = self._make_appearance_dict(capacity_list)
             two_max_number = sorted(set(capacity_list), reverse=True)[0:2]
             sub_between_list = two_max_number[0] - two_max_number[1]
-            sub_between_num = sum_of_weight - weight_maximum
+            sub_between_num = current_weight - maximum_weight
             real_to_sub = min(sub_between_list, sub_between_num)
             sub_operate_list = self._split_download_size(real_to_sub, number_appearance_dict[two_max_number[0]])
             for index in range(len(sub_operate_list)):
                 capacity_list[index] -= sub_operate_list[index]
             capacity_list.sort(reverse=True)
-            sum_of_weight = sum(capacity_list)
+            current_weight = sum(capacity_list)
         return capacity_list
 
     def _generate_new_section_with_capacity(self, section_list, granted_list):
         result_list = list()
         for index in range(len(section_list)):
-            operate_item = section_list[index]
-            operate_count = granted_list[index]
-            result_list.extend(self._split_download_section(operate_item, operate_count))
+            current_section, thread_count = section_list[index], granted_list[index]
+            result_list.extend(self._split_download_section(current_section, thread_count))
         return sorted(result_list, key=lambda x: x[0])
 
     def _split_download_section(self, current_section, thread_count: int):
-        content_size = current_section[1] - current_section[0] + 1
-        each_section_size = self._split_download_size(content_size, thread_count)
-        current_position = current_section[0]
-        result_list = list()
-        for item in each_section_size:
-            end_position = current_position + item
-            result_list.append([current_position, end_position - 1])
-            current_position = end_position
-        return result_list
+        content_size = self._get_section_size(current_section)
+        section_size_list = self._split_download_size(content_size, thread_count)
+        return self._generate_sub_section(current_section, section_size_list)
+
+    @staticmethod
+    def _format_final_section_count(section_length, expect_length):
+        return max(section_length, expect_length)
 
     @staticmethod
     def _make_appearance_dict(number_list):
@@ -68,11 +62,25 @@ class SectionMaker(object):
         return result_dict
 
     @staticmethod
+    def _get_section_size(section_item):
+        return section_item[1] - section_item[0] + 1
+
+    @staticmethod
     def _split_download_size(content_size, thread_count):
         low_base_size = content_size // thread_count
         content_size_list = [low_base_size] * thread_count
-        height_size_count = content_size - low_base_size * thread_count
+        height_size_count = content_size % thread_count
         content_size_list[0:height_size_count] = [low_base_size + 1] * height_size_count
         while 0 in content_size_list:
             content_size_list.remove(0)
         return content_size_list
+
+    @staticmethod
+    def _generate_sub_section(current_section, section_size_list):
+        result_list = list()
+        current_position = current_section[0]
+        for section_size in section_size_list:
+            end_position = current_position + section_size
+            result_list.append([current_position, end_position - 1])
+            current_position = end_position
+        return result_list
